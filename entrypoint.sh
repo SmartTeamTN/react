6d50c2ece1a612314e9a91c67bb91d2522a25fb3
Walid MNASRI <mnasri.walid@gmail.com>
2018-03-05 14:32:39 +0100

    install webpack in enrtrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-if [ ! -d node_modules ]; then
-    npm install
-else
-    npm update
-fi
+npm install --save-dev webpack@3.4.1
+npm install --save-dev webpack-dev-server@2.6.1
+npm install --save-dev webpack-cli@2.0.10
+
+npm install
 
 npm start
